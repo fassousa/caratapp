41241b265dfcc848d1324a67555ca20f8c3380f7
johnhaus <john.haus@outlook.com>
2018-08-21 17:31:56 -0300

    Create routes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :companies
-  devise_for :users
   root to: 'pages#home'
+  resources :employees
+  get 'dashboard', to: 'employees#dashboard', as: :dashboard
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -56,18 +56,6 @@ ActiveRecord::Schema.define(version: 2018_08_21_175546) do
     t.index ["company_id"], name: "index_routes_on_company_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "employees", "companies"
   add_foreign_key "routes", "companies"
 end
