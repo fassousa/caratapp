f5651ca990e9e579a5ef263f0a14c23fdd002f19
johnhaus <john.haus@outlook.com>
2018-08-22 18:41:06 -0300

    added modal to dashboard for add employee function

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,6 +3,7 @@ class EmployeesController < ApplicationController
   def dashboard #this is our index page
     @user = current_user
     @employees = @user.employees
+    @employee = Employee.new
 
     @employees = Employee.where.not(latitude: nil, longitude: nil)
 
@@ -13,6 +14,24 @@ class EmployeesController < ApplicationController
         # infoWindow: { content: render_to_string(partial: "/employees/map_box", locals: { flat: flat }) }
       }
     end
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    @employee.user = current_user
+
+    if @employee.save
+      respond_to do |format|
+        format.html { redirect_to user_path(current_user) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'employees/dashboard' }
+        format.js  # <-- idem
+      end
+    end
+
   end
 
   def edit
