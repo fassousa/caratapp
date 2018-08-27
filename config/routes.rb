4b5023c6a873fe2cf2c1eafab043607f323ef314
Julia Campana <juliarcampana@gmail.com>
2018-08-27 18:44:06 -0300

    rewards and dashboard layout adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'dashboard', to: 'employees#dashboard', as: :dashboard
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'rewards', to: 'pages#rewards'
+
   authenticated :user do
     root 'employees#dashboard', as: :authenticated_root
   end
