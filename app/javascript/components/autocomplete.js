12c0bccbd94407a7c8536f2a33d6716589335352
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-08-29 14:51:51 -0300

    Create autocomplete feature for the adress box inside the sign up page

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -2,6 +2,7 @@ function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
     var inputAddressAdd = document.getElementById('employee_address_add');
     var inputAddressEdit = document.getElementById('employee_address_edit');
+    var inputAdressSign = document.getElementById('user_address');
 
     if (inputAddressAdd) {
       var autocomplete = new google.maps.places.Autocomplete(inputAddressAdd, { types: [ 'geocode' ] });
@@ -15,6 +16,15 @@ function autocomplete() {
     if (inputAddressEdit) {
       var autocomplete = new google.maps.places.Autocomplete(inputAddressEdit, { types: [ 'geocode' ] });
       google.maps.event.addDomListener(inputAddressEdit, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+
+    if (inputAdressSign) {
+      var autocomplete = new google.maps.places.Autocomplete(inputAdressSign, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(inputAdressSign, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
         }
