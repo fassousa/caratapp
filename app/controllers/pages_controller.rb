7c859096434adefe8065c3a5ed26dd59af1a11cf
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-08-30 12:12:19 -0300

    Create methods to calculate total distance and price

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,12 @@ class PagesController < ApplicationController
   end
 
   def rewards
+    @annual_cost = Money.new(430920, "BRL")
+    @total_employees = current_user.employees.count
+    @total_cost = @annual_cost * @total_employees
+    @total_individual_distance = 0
+    current_user.employees.each do |e|
+      @total_individual_distance += e.distance_to_work
+    end
   end
 end
