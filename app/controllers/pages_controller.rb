5de7c8b9c9bab1d94d44934ef31d05a5d518b9e6
Julia Campana <juliarcampana@gmail.com>
2018-08-30 17:34:00 -0300

    changes from presentation feedback

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,17 +5,17 @@ class PagesController < ApplicationController
   end
 
   def rewards
-    @route = Route.new(current_user)
+    # @route = Route.new(current_user)
 
-    # @employees = current_user.employees
+    # # @employees = current_user.employees
 
-    @route.carat_cost
+    # @route.carat_cost
 
 
 
-    @total_individual_distance = 0
-    current_user.employees.each do |e|
-      @total_individual_distance += e.distance_to_work
-    end
+    # @total_individual_distance = 0
+    # current_user.employees.each do |e|
+    #   @total_individual_distance += e.distance_to_work
+    # end
   end
 end
